4cf0c54ee5eac96c32bbebb23536dcef14c237a6
G3TSN1P3D <chava4flores@hotmail.com>
2022-07-14 13:56:31 -0400

    Added functionality to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,9 +2,65 @@ const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
 module.exports = {
-  async getSingleUser() {},
-  async createUser() {},
-  async login() {},
-  async savePlayer() {},
-  async removePlayer() {},
+  async getSingleUser({ user = null, params }, res) {
+    const foundUser = await User.findOne({
+      $or: [{ _id: user ? user.id : params.id }, { email: params.email }],
+    });
+
+    if (!foundUser) {
+      return res.status(404).json({ message: 'Could not find this user!' });
+    }
+
+    res.json(foundUser);
+  },
+  async createUser({ body }, res) {
+    const user = await User.create(body);
+
+    if (!user) {
+      return res.status(400).json({ message: 'Something is wrong!' });
+    }
+    const token = signToken(user);
+    res.json({ user, token });
+  },
+  async login({ body }, res) {
+    const user = await User.findOne({ email: body.email  });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const correctPw = await user.isCorrectPassword(body.password);
+    if (!correctPw) {
+      // for testing purpouses if password is not correct, it will throw "Wrong password"
+      // !NEEDS to be changed to "Incorrect email or password!" in the future
+      return res.status(400).json({ message: 'Wrong password' });
+    }
+    const token = signToken(user);
+    res.json({ user, token });
+  },
+  async savePlayer({ user, params, body }, res) {
+    console.log(user);
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { $or: [{ _id: user ? user.id : params.id }, { email: params.email }] },
+        { $addToSet: { savedPlayers: body } },
+        { new: true, runValidators: true }
+      );
+      return res.json(updatedUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  async removePlayer({ user, params }, res) {
+    const updatedUser = await User.findOneAndUpdate(
+      { $or: [{ _id: user ? user.id : params.id }, { email: params.email }] },
+      { $pull: { savedPlayers: { _id: params._id } } },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    return res.json(updatedUser);
+  },
 };

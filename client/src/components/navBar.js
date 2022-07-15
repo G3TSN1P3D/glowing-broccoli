fa02b9e8c10554be06b526a66094159d6fa36d80
Gorilla Jones <kgorski90lol@gmail.com>
2022-07-15 09:14:15 -0500

    Please save these

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const navBar = styled.div`
-  
-`
+export default function navBar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                
+            </div>
 
-
-export default navBar
+        </nav>
+  );
+}

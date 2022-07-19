e3dc7103ec7166ef05dbbb81be4c0bf18265ed7c
G3TSN1P3D <chava4flores@hotmail.com>
2022-07-19 12:19:52 -0400

    Finished the resolvers for the backend

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -68,9 +68,11 @@ const typeDefs = gql`
       first_name: String
       last_name: String
       email: String
-      password: String): Auth
+      password: String
+    ): Auth
     login(email: String, password: String): Auth
     newPlayer(playerId: ID, input: NewPlayerInput): Player
+    editPlayer(playerId: ID! input: NewPlayerInput): Player
     removePlayer(playerId: ID!): Player
     newStat(playerId: ID, input: NewStatInput): Player
     editStat(playerId: ID!, statId: ID, input: NewStatInput): Player
